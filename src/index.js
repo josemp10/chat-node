9974400dfd02eb7ebabfa96e57e08213b5cffcc3
josemp10 <josemp10@gmail.com>
2019-05-11 06:24:06 -0700

    First skeleton

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoyPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoyPath))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New websocket connection')
+
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has join')
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profenity is not allowed')
+        }
+        io.emit('message', message)
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left')
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
+
